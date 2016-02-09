082eec543684450ad52f9ed30203b1ebf49bbaf7
englep10 <englep10@gmail.com>
2016-02-08 21:00:30 -0500

    signals ex1

--- SamplingSignals.elm
+++ SamplingSignals.elm
@@ -2,7 +2,9 @@ import Graphics.Element exposing (..)
 import Mouse
 import Time
 
+clickPosition : Signal (Int, Int)
+clickPosition =
+  Signal.sampleOn Mouse.clicks Mouse.position
 
 main : Signal Element
 main =
-
